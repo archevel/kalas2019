1784d43432a3041e2ac09e31975e700d5081a85f
Emil Hellman <emil.hellman@gmail.com>
2019-01-27 17:09:57 +0100

    add logic

--- index.md
+++ index.md
@@ -3,6 +3,8 @@ Forsta ledtraden finns [har](http://maps.google.com/maps?q=57.7141994,11.9025116
 
 ## Vad ar svaret pa forsta fragan?
 
-<div id="text">
-no script ran...
-</div>
+<div id="q1">
+    <input id="a1" type="text" />
+    <button id="sub1">Kontrollera svar</button
+    <a id="link1" href="./q2">Till nasta!</a>
+</div
--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 $().ready(function() {
-    $("#text").html("Text added by jQuery code.");
+    $("#link1").hide();
+    $("#sub1").click(function() {
+        if($("#a1")[0].value === "stone") {
+            alert("Rätt");
+            $("link1").show();
+        } else {
+            alert("Fel! Försök igen :)");
+        }
+    });
 });
