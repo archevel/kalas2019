2e28dfc83e1d2a79295c88f1a2dc26a75260eda7
Emil Hellman <emil.hellman@gmail.com>
2019-01-30 06:33:42 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 $().ready(function() {
 
     $("#sub").click(function() {
-        var actualAnswer = $("#a").value().toLowerCase();
+        var actualAnswer = $("#a").val().toLowerCase();
         var acceptedAnswers = $("#a").attr('data-expect').toLowerCase().split("|");
         
         if(acceptedAnswers.indexOf(actualAnswer) >= 0) {
