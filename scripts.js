5ba50af1442603e61fc14a47ce33639ce9ee745d
Emil Hellman <emil.hellman@gmail.com>
2019-01-30 06:35:39 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ $().ready(function() {
 
     $("#sub").click(function() {
         var actualAnswer = $("#a").val().toLowerCase();
-        var acceptedAnswers = $("#a").attr('data-expect').toLowerCase().split("|");
+        var acceptedAnswers = $("#a").attr('data-expect').toLowerCase().split(",");
         
         if(acceptedAnswers.indexOf(actualAnswer) >= 0) {
             alert("Korrekt!");
